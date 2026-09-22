5ad07e02983f1d909204a3985e4e6c7ec7648935
El Mehdi Nasad <nasad.ouarit.mehdi@gmail.com>
2026-09-22 22:30:33 +0000

    ajout des fichier index.php et style.css et a-propos.php et le dossier image pour debuter le devellopement du site web photographie

---
+++ a-propos.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>À propos - Robbie Lens Photographie</title>
+        <link href="style.css" 
+              type="text/css" 
+              rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <a href="index.php">
+                <img src="image/logo.png" 
+                     alt="Logo Robbie Lens Photographie">
+            </a>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Accueil</a></li>
+                    <li><a href="a-propos.php">À propos</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <h1>À propos de Robbie Lens</h1>
+            <p>
+            Photographe depuis plus de 5 ans, 
+            je réalise des reportages aux photos 
+            dynamiques et pertinentes pour vos projets 
+            de communication. Créativité, qualité, 
+            et sérénité pour vous ! Je gère tout, 
+            depuis la direction artistique, 
+            la réalisation du reportage, jusqu’à 
+            la livraison de vos photos retouchées, 
+            prêtes à l’emploi.
+            </p>
+            <h2>Services</h2>
+            <ul>
+                <li>Portrait seul ou à plusieurs,</li>
+                <li>Shooting mode,</li>
+                <li>Retouches sur mesure,</li>
+                <li>Développement.</li>
+            </ul>
+        </main>
+        <footer>
+            <a href="index.php">
+                <img src="image/logo.png" 
+                     alt="Logo Robbie Lens Photographie">
+            </a><br>
+            <a href="https://x.com" 
+            target="_blank"><img src="image/twitter.png" alt="X">
+            </a>
+            <a href="https://www.instagram.com" 
+            target="_blank"><img src="image/instagram.png" 
+                                 alt="Instagram"></a>
+            <p>© 2026 Robbie Lens Photographie</p>
+        </footer>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Accueil - Robbie Lens Photographie</title>
+        <link href="style.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <a href="index.php">
+                <img src="image/logo.png" 
+                     alt="Logo Robbie Lens Photographie">
+            </a>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Accueil</a></li>
+                    <li><a href="a-propos.php">À propos</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <h1>Photographie</h1><br>
+            <img src="image/robbie-lens.png" 
+                 alt="Photo de Robbie Lens">
+            <p>
+                Où professionalisme s’allie avec passion. 
+                Depuis plus de 5 ans maintenant, 
+                j’exerce mon métier avec la passion 
+                qui m’anime : capturer l’essence de chaque 
+                instant.
+            </p>
+        </main>
+        <footer>
+            <a href="index.php">
+                <img src="image/logo.png" 
+                     alt="Logo Robbie Lens Photographie">
+            </a><br>
+            <a href="https://x.com" 
+            target="_blank"><img src="image/twitter.png" 
+                                alt="X"></a>
+            <a href="https://www.instagram.com" 
+               target="_blank"><img src="image/instagram.png" 
+                                    alt="Instagram"></a>
+            <p>© 2026 Robbie Lens Photographie</p>
+        </footer>
+    </body>
+</html>
